b4ff5eed020262d80148f5a5bd234a03aed6bde8
Stephen Wei <log9527@gmail.com>
2025-06-27 01:28:29 +0800

    some additional explains

--- for_loop.py
+++ for_loop.py
@@ -6,24 +6,24 @@ BiliBili = [
         ["SevenHills", "During", "Dough"]]]
 
 
-# for characters in BiliBili:
-#     if isinstance(characters, list):
-#         for host in characters:
-#             if isinstance(host, list):
-#                 for husbands in host:
-#                     print("Darlings:", husbands)
+""" for characters in BiliBili:
+        if isinstance(characters, list):
+            for host in characters:
+                if isinstance(host, list):
+                    for husbands in host:
+                        print("Darlings:", husbands)
 
-#             else:
-#                 print("host:", host)
+                else:
+                    print("host:", host)
 
-#     else:
-#         print("roles:", characters)
+        else:
+            print("roles:", characters)   """    # 三个双引号 可以注释多行代码
 
 
-# writing with recursion function
-def print_nested_list(somelist):  # 参数不能使用list，因为list是Python的保留字
+# writing with recursion function (generalized for nested lists)
+def print_nested_list(somelist):  # 參數不能使用list，因為list是Python的保留字
     for each_item in somelist:
-        if isinstance(each_item, list):  # list是类型
+        if isinstance(each_item, list):  # list是類型(型別)，isinstance第二個參數放的是類型(型別)
             print_nested_list(each_item)  # Recursive call
         else:
             print(each_item)
@@ -34,4 +34,4 @@ def print_nested_list(somelist):  # 参数不能使用list，因为list是Python
 
 # as a function
 
-print_nested_list(BiliBili)
+print_nested_list(somelist)
